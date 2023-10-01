7afd902d132acd67840636bed92705a84f446b28
Alex Suilea <alexsuilea21@gmail.com>
2023-10-01 15:33:14 +0300

    Sudoku

--- hashtable/MainHashTable.java
+++ hashtable/MainHashTable.java
@@ -17,7 +17,31 @@ public class MainHashTable {
 //        RansomNote ransomNote = new RansomNote();
 //        System.out.println(ransomNote.canConstruct("aa","ab"));
 //        FindTheSecondLetterInAString.SecondLetter("suilea mihai alexandru");
-        TopKElement topKElement = new TopKElement();
-        System.out.println(Arrays.toString(topKElement.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
+//        TopKElement topKElement = new TopKElement();
+//        System.out.println(Arrays.toString(topKElement.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
+
+        ValidSudoku validSudoku = new ValidSudoku();
+        char[][] board = new char[][]
+                {{'5','3','.','.','7','.','.','.','.'}
+                ,{'6','.','.','1','9','5','.','.','.'}
+                ,{'.','9','8','.','.','.','.','6','.'}
+                ,{'8','.','.','.','6','.','.','.','3'}
+                ,{'4','.','.','8','.','3','.','.','1'}
+                ,{'7','.','.','.','2','.','.','.','6'}
+                ,{'.','6','.','.','.','.','2','8','.'}
+                ,{'.','.','.','4','1','9','.','.','5'}
+                ,{'.','.','.','.','8','.','.','7','9'}};
+        System.out.println(validSudoku.isValidSudoku2(board));
+        char[][] board2 = new char[][]
+                {{'8','3','.','.','7','.','.','.','.'}
+                ,{'6','.','.','1','9','5','.','.','.'}
+                ,{'.','9','8','.','.','.','.','6','.'}
+                ,{'8','.','.','.','6','.','.','.','3'}
+                ,{'4','.','.','8','.','3','.','.','1'}
+                ,{'7','.','.','.','2','.','.','.','6'}
+                ,{'.','6','.','.','.','.','2','8','.'}
+                ,{'.','.','.','4','1','9','.','.','5'}
+                ,{'.','.','.','.','8','.','.','7','9'}};
+        System.out.println(validSudoku.isValidSudoku2(board2));
     }
 }
