0434b05677f95b2f0c5ebe8f676fbd529eeeefe0
Alex Suilea <alexsuilea21@gmail.com>
2023-10-03 22:20:59 +0300

    roadmap

---
+++ roadmap/src/arraysandhashing/MainArrHs.java
@@ -0,0 +1,8 @@
+package arraysandhashing;
+
+public class MainArrHs {
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        System.out.println(containsDuplicate.containsDuplicate(new int[]{1,2,3,1}));
+    }
+}
