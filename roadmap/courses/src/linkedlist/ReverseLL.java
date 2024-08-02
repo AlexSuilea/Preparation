303a5efe7975a860c77229e1257709c5266c2d59
Alex Suilea <alexsuilea21@gmail.com>
2024-08-02 18:10:35 +0300

    LL

---
+++ roadmap/courses/src/linkedlist/ReverseLL.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+public class ReverseLL {
+    public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode nextCurrent = null;
+
+        while(current != null) {
+            nextCurrent = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextCurrent;
+        }
+        return previous;
+    }
+}
