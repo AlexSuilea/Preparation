81f64028f407e623361a7dea342c1f84a00f23d4
Alex Suilea <alexsuilea21@gmail.com>
2024-08-04 22:55:07 +0300

    refactoring

--- roadmap/courses/src/Main.java
+++ roadmap/courses/src/Main.java
@@ -1,18 +1,4 @@
-import stack.BaseballGame;
-
 public class Main {
     public static void main(String[] args) {
-        String[] operations = {"5","2","C","D","+"};
-        System.out.println(BaseballGame.calPoints(operations));
-        String[] operations1 = {"5","-2","4","C","D","9","+","+"};
-        System.out.println(BaseballGame.calPoints(operations1));
-
-//        System.out.println(Arrays.toString(ConcatenationOfArray.getConcatenation(new int[]{1, 2, 1})));
-//        System.out.println(Arrays.toString(ConcatenationOfArray.getConcatenation(new int[]{1, 3, 2, 1})));
-
-//        System.out.println(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[] {1,1,2}));
-//        System.out.println(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
-//        System.out.println(RemoveElement.removeElement(new int[] {3,2,2,3},3));
-//        System.out.println(RemoveElement.removeElement(new int[] {0,1,2,2,3,0,4,2},2));
     }
 }
